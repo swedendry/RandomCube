b2e75f6cf3d89763e9e2c81653a2b3fe168d15b6
thegido <thegido@wisecat.co.kr>
2020-04-18 02:45:19 +0900

    cube move

--- Client/Assets/Scripts/Entity/Cubes/Cube.cs
+++ Client/Assets/Scripts/Entity/Cubes/Cube.cs
@@ -4,31 +4,43 @@ public class Cube : Entity
 {
     public float speed = 5.0f;
     private Vector3 target;
-    private Animation anim;
+    private Animator anim;
+    private bool isMove;
 
     private void Start()
     {
         target = transform.position;
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if(isMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+
+        if(isMove && transform.position == target)
+        {
+            isMove = false;
+            anim.SetBool("Move", false);
+        }
     }
 
     public void Selected()
     {
-        anim.Play("Cube_Selected");
+        anim.SetBool("Selected", true);
     }
 
     public void DeSelected()
     {
-        anim.Stop();
+        anim.SetBool("Selected", false);
     }
 
     public void Move(Vector3 target)
     {
         this.target = target;
+        isMove = true;
+        anim.SetBool("Move", true);
     }
 }
