76d593b7b493a7ee202c073a2edf55e7c29e35b6
thegido <thegido@wisecat.co.kr>
2020-04-18 16:43:50 +0900

    path test

--- Client/Assets/Scripts/Entity/Monsters/Monster.cs
+++ Client/Assets/Scripts/Entity/Monsters/Monster.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class Monster : Entity
 {
-    private Transform[] paths;
+    private Transform[] path;
+
     public void Move(List<GameObject> paths)
     {
-        this.paths = paths.Select(x => x.transform).ToArray();
-        iTween.PutOnPath(gameObject, this.paths, 0f);
+        this.path = paths.Select(x => x.transform).ToArray();
+        iTween.MoveTo(gameObject, iTween.Hash("path", path, "time", 10, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop, "movetopath", false));
+        //iTween.PutOnPath(gameObject, this.paths, 0f);
         //iTween.PointOnPath(paths.Select(x => x.transform).ToArray(), 1f);
-    }
-
-    private void OnDrawGizmos()
-    {
-        // Gizmos.DrawLine 는 직선으로 선을 그립니다.
-        // iTween.DrawPath 는 곡선형태로 선을 그립니다.
-        // iTween.DrawPath 로 그리는 라인과 iTween.PutOnPath 로 이동하는 동선이 같습니다.
-        iTween.DrawPath(paths, Color.magenta);
-        Gizmos.color = Color.black;
-        //Gizmos.DrawLine(transform.position, lookTarget.transform.position);
     }
 }
