dd452acc688bd3ca3fa185358c5ffeafe0924dd0
thegido <thegido@wisecat.co.kr>
2020-04-27 22:04:00 +0900

    lobby

---
+++ Client/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Renderer renderer;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1f);
+        }
+    }
+}
